c2f85f15c63f7e9aceea5b729bcba089055ac524
MiltonStivenPinzon1223 <miltonstivenpinzon@gmail.com>
2025-03-04 20:12:56 -0500

    first commit

---
+++ database/migrations/2025_01_24_085455_create_loans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contacts_id');
+            $table->foreignId('users_id');
+            $table->float('amount');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+};
---
+++ database/migrations/2025_01_24_085528_create_loan_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contacts_id');
+            $table->foreignId('users_id');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_payments');
+    }
+};
