4274c16cf17b827329735f5cdc93728683510826
Andres Abril <abril0756@gmail.com>
2025-06-09 22:08:36 -0500

    Boton de mas en productos

--- resources/views/sale_pos/partials/pos_form.blade.php
+++ resources/views/sale_pos/partials/pos_form.blade.php
@@ -60,7 +60,7 @@
 					@endif
 					
 
-					<button type="button" class="btn btn-default bg-white btn-flat pos_add_quick_product" data-href="{{action([\App\Http\Controllers\ProductController::class, 'quickAdd'])}}" data-container=".quick_add_product_modal"><i class="fa fa-plus-circle text-primary fa-lg"></i></button>
+					{{-- <button type="button" class="btn btn-default bg-white btn-flat pos_add_quick_product" data-href="{{action([\App\Http\Controllers\ProductController::class, 'quickAdd'])}}" data-container=".quick_add_product_modal"><i class="fa fa-plus-circle text-primary fa-lg"></i></button> --}}
 				</span>
 			</div>
 		</div>
